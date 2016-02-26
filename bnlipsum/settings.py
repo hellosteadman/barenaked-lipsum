7b1cd8eec2059e56aa7511f77b3142c8bed2c0fd
Mark <mark@steadman.io>
2016-02-26 22:18:10 +0000

    Use Whitenoise for static files

--- bnlipsum/settings.py
+++ bnlipsum/settings.py
@@ -94,6 +94,8 @@ USE_TZ = True
 
 # Static files
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Redis
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
